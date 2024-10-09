43668c0b02dafca707d8a3129a7f9bba6ad22774
sdishehab <150367787+shehab097@users.noreply.github.com>
2024-10-10 03:13:17 +0600

    c++ updated

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello world" << endl;
+    cout << "I am 21 year old" << endl;
+    for (int i = 0;i < 10;i++) {
+        cout << i << endl;
+    }
+    return 0;
+}
